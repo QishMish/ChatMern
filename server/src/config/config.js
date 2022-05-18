bbca952511f95388ba77926906841cb6fafe2bda
misho <mishikoqajaia50@gmail.com>
2022-05-18 22:23:45 +0400

    Change config

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,19 +1,20 @@
 require("dotenv").config();
 
 const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, NODE_ENV } = process.env;
+
 module.exports = {
   development: {
-    username: "postgres",
-    password: "postgres",
-    database: "chat",
-    host: "localhost",
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
     dialect: "postgres",
   },
   test: {
-    username: "postgres",
-    password: "postgres",
-    database: "chat",
-    host: "localhost",
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
     dialect: "postgres",
   },
   production: {
